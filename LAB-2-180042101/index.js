76aaeb2a430f5a600a15fdbeae6463f0dd82577a
Fahim <fahimabrar191336@gmail.com>
2021-06-27 20:19:20 +0600

    Database Connected

--- LAB-2-180042101/index.js
+++ LAB-2-180042101/index.js
@@ -2,6 +2,23 @@ const app = require('./app');
 
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGOURL,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+
+})
+.then(()=> {
+    console.log('Database connected');
+})
+.catch((err)=> {
+    if(err){
+        console.log('Database connection failed');
+    }
+})
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
